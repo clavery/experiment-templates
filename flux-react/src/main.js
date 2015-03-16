05f0131357ddcd8d21ffb580bdda257a1330d701
Charles Lavery <charles.lavery@gmail.com>
2015-03-15 23:32:19 -0400

    debugging helpers

--- flux-react/src/main.js
+++ flux-react/src/main.js
@@ -1,6 +1,7 @@
 var React = require('react');
-var style = require('./style.scss');
 var App = require('./app');
+
+var style = require('./style.scss');
 
 // render app (router component) into page
 React.render(React.createElement(App), document.body);
@@ -9,6 +10,10 @@ React.render(React.createElement(App), document.body);
 if (DEBUG) {
   global.React = React;
   global.Dispatcher = require('./dispatcher');
+  global.TodoClient = require('./clients/todo');
+  global.TodoStore = require('./stores/todo');
+  var {TodoActionCreators} = require('./actions');
+  global.TodoActionCreators = TodoActionCreators;
 }
 
 
