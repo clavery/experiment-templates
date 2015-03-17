63609b22099626b35ec7e3989abc598df45dc3de
Charles Lavery <charles.lavery@gmail.com>
2015-03-17 00:12:22 -0400

    make tests faster

--- flux-react/testpreprocessor.js
+++ flux-react/testpreprocessor.js
@@ -1,7 +1,9 @@
 // preprocessor.js
 var ReactTools = require('react-tools');
+var nodemodule = /node_modules/;
 module.exports = {
-  process: function(src) {
+  process: function(src, file) {
+    if (nodemodule.test(file)) return src;
     return ReactTools.transform(src, {harmony:true});
   }
 };
