130da472f22117bf4b5b5a2c46e762189530af91
Charles Lavery <charles.lavery@gmail.com>
2015-03-16 22:51:56 -0400

    cleaner production config

--- flux-react/webpack.config.js
+++ flux-react/webpack.config.js
@@ -2,19 +2,19 @@ var webpack = require('webpack');
 var path = require('path');
 
 var environment = process.env.NODE_ENV;
-var isProduction = (environment === "production");
+var production = (environment === "production");
 
 var ENTRIES = ['./src/main.js'];
 
 BOOSTRAP_PATH="./node_modules/bootstrap-sass/assets/stylesheets/"
 
-module.exports = {
-  devtool: isProduction ? false : '#inline-source-map',
+var config = {
+  devtool: '#inline-source-map',
   entry: {
     app: ENTRIES
   },
   output: {
-    pathinfo: !isProduction,
+    pathinfo: true,
     publicPath: '/dist/',
     path: __dirname + "/dist",
     filename: 'main.js',
@@ -25,11 +25,10 @@ module.exports = {
       { test: /\.jsx$/, exclude: /node_modules/, loaders: ['react-hot', 'jsx-loader?harmony']},
       { test: /\.json$/, loaders: ['json-loader']},
       { test: /\.css$/, exclude: /node_modules/, loaders: ['style', 'css?sourceMap']},
-      { 
-        test: /\.scss$/, exclude: /node_modules/, loaders: ['style', 'css',
-        'sass?sourceMap=map&outputStyle=expanded&includePaths[]=' + (path.resolve(__dirname, BOOSTRAP_PATH))
-      ]},
-      { test: /\.woff2?$/,   loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.scss$/, exclude: /node_modules/, loaders: ['style', 'css',
+        'sass?sourceMap=map&outputStyle=expanded&includePaths[]=' +
+          (path.resolve(__dirname, BOOSTRAP_PATH)) ]},
+      { test: /\.woff2?$/,   loader: "file-loader" },
       { test: /\.ttf$/,    loader: "file-loader" },
       { test: /\.eot$/,    loader: "file-loader" },
       { test: /\.svg$/,    loader: "file-loader" }
@@ -38,5 +37,21 @@ module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx'],
     root: [path.join(__dirname, "lib")]
-  }
+  },
+  plugins: []
 };
+
+// production "build" options
+if (production) {
+  config.debug = false;
+  config.bail = true;
+  config.devtool = false;
+  config.profile = true;
+  config.output.pathinfo = false;
+
+  config.plugins = config.plugins.concat([
+    new webpack.optimize.DedupePlugin(),
+  ]);
+}
+
+module.exports = config
