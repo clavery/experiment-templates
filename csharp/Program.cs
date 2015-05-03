06d831734ee4cf4e18abea68da7a44df0d7cad86
Charles Lavery <charles.lavery@gmail.com>
2015-05-03 16:02:17 -0400

    csharp template

---
+++ csharp/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IList<String> strings = new List<String>() {
+                "One",
+                "Two",
+                "Three",
+                "Four",
+                "Five"
+            };
+        }
+    }
+}
